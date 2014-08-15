6a86cd174542a97386ff0c7f85df5df6da4b225e
animir <animir@Setori>
2014-08-15 23:41:27 +1100

    AbstractParser. Phpdoc

--- src/Animir/Alltables/Parser/AbstractParser.php
+++ src/Animir/Alltables/Parser/AbstractParser.php
@@ -3,6 +3,13 @@ namespace Animir\Alltables\Parser;
 
 use Animir\Alltables\ProjectOptions;
 
+/**
+ * Abstract class for any parser
+ * 
+ * @package alltables
+ * @author animir <animir@ya.ru>
+ */
+
 abstract class AbstractParser {
 
     protected $options;
@@ -11,20 +18,37 @@ abstract class AbstractParser {
      * @var Animir\Alltables\Resource\Resource
      */
     protected $resource;
-    
+    /**
+     * Parse resource with config from ProjectOptions class and return data
+     * 
+     * @return array
+     */
     abstract public function parse();
     
-    protected function filterRowByConfig($array) {
-        if (!isset($this->options['header'])) return $array;
+    
+    /**
+     * Filter $array according to 'header' param in ProjectOptions
+     * 
+     * @param array $data
+     * @return array
+     */
+    protected function filterRowByConfig(array $data) {
+        if (!isset($this->options['header'])) return $data;
         
         $resultArray = [];
         foreach($this->options['header'] as $field => $val ) {
-            $resultArray []= isset($array[$field]) ? $array[$field] : null ;            
+            $resultArray []= isset($data[$field]) ? $data[$field] : null ;            
         }
 
         return $resultArray;
     }
     
+    /**
+     * Store $data to file with filename from ProjectOptions
+     * 
+     * @param array $data
+     * @return bool
+     */
     public function store(array $data) {
         $options = ProjectOptions::getSpecOptions('alltables');
         $file = fopen($options['save_path'] . $this->options['name'] . '.allt', 'wb');
@@ -32,7 +56,14 @@ abstract class AbstractParser {
             fputcsv($file, $row);
         }
         fclose($file);
+        return true;
     }
+    
+    /**
+     * Load data from file with filename from ProjectOptions
+     * 
+     * @return array
+     */
     
     public function load() {
         $data = [];
@@ -45,6 +76,12 @@ abstract class AbstractParser {
         return $data;
     }
     
+    /**
+     * Get array with parsed data
+     * which loaded from file if it exists
+     * 
+     * @return array
+     */
     public function getArray() {
         if ($this->parsedDataExists() === false) {
             $data = $this->parse();
@@ -55,19 +92,47 @@ abstract class AbstractParser {
         return $data;
     }
     
+    /**
+     * Exists or not file with parsed data
+     * 
+     * @return bool
+     */
+    
     public function parsedDataExists() {
         return file_exists(ProjectOptions::getSpecOptions('alltables')['save_path'] . $this->options['name'] . '.allt');
     }
     
+    /**
+     * Return resource
+     * @return \Animir\Alltables\Resource\Resource
+     */
     public function getResource() {
         return $this->resource;
     }
+    
+    /**
+     * Set resource
+     * @param \Animir\Alltables\Resource\Resource $resource
+     */
     public function setResource(\Animir\Alltables\Resource\Resource $resource){
         $this->resource = $resource;
     }
+    
+    /**
+     * Get parser options
+     * 
+     * @return array
+     */
     public function getOptions() {
         return $this->options;
     }
+    
+    /**
+     * Set options for parser
+     * 
+     * @param array|string $options
+     */
+    
     public function setOptions($options) {
         if (!is_array($options)) {
             $this->options = (array)$options;
